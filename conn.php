9fc2658bb9aacd1e6b8ac89a457e1d043d0ab5d0
MasayuNisaAtika <masayunisaatika19@gmail.com>
2025-07-19 19:32:47 +0700

    update conection

--- conn.php
+++ conn.php
@@ -1,12 +1,17 @@
-<?php 
-    $host = "sql.freedb.tech";
-    $user = "freedb_labkom";
-    $pass = "sZRM%NjWy\$xDk7Z";  // tanda $ perlu di-escape
-    $db   = "freedb_labkompis";
-    $port = 3306;
+<?php
+$host = "sql.freedb.tech";
+$user = "freedb_labkom";
+$pass = "sZRM%NjWy\$xDk7Z";  // tanda $ perlu di-escape
+$db   = "freedb_labkompis";
+$port = 3306;
 
-    $conn    = mysqli_connect($host, $user, $password, $dbname);
-    if (mysqli_connect_errno()) {
-        die("Koneksi Gagal Karena : ". mysqli_connect_error());
-    }
+// Koneksi ke MySQL
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
+
+// echo "Koneksi berhasil";
 ?>
