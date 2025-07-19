9be45b11acb1144a4d4b1e879fbe478ca93a119e
MasayuNisaAtika <masayunisaatika19@gmail.com>
2025-07-19 18:59:22 +0700

    update connect

--- conn.php
+++ conn.php
@@ -1,8 +1,9 @@
 <?php 
-    $host       = "localhost";
-    $user       = "root";
-    $password   = "";
-    $dbname     = "db_labkom";
+    $host = "sql.freedb.tech";
+    $user = "freedb_labkom";
+    $pass = "sZRM%NjWy\$xDk7Z";  // tanda $ perlu di-escape
+    $db   = "freedb_labkompis";
+    $port = 3306;
 
     $conn    = mysqli_connect($host, $user, $password, $dbname);
     if (mysqli_connect_errno()) {
